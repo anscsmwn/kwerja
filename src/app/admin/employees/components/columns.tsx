58aa3480f7e5b9cae2495bb8ef2cc4f1bfe0d323
Annas Casmawan Ahmad <annasnganro.dev@gmail.com>
2023-09-19 19:37:43 +0800

    fix: no such file or directory path

--- src/app/admin/employees/components/columns.tsx
+++ src/app/admin/employees/components/columns.tsx
@@ -42,6 +42,21 @@ export const columns: ColumnDef<Task>[] = [
     enableHiding: false,
   },
   {
+    accessorKey: 'employee',
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Employee" />
+    ),
+    cell: ({ row }) => {
+      return (
+        <div className="flex space-x-2">
+          <span className="max-w-[500px] truncate font-medium">
+            {row.getValue('employee')}
+          </span>
+        </div>
+      )
+    },
+  },
+  {
     accessorKey: 'title',
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Title" />
--- src/app/admin/employees/data/seed.ts
+++ src/app/admin/employees/data/seed.ts
@@ -1,23 +1,27 @@
 import fs from 'fs'
 import path from 'path'
 import { faker } from '@faker-js/faker'
+
 import { labels, priorities, statuses } from './data'
+
+const outputDirectory = path.join(__dirname, 'output')
+const outputPath = path.join(outputDirectory, 'tasks.json')
+// Check if the output directory exists, and create it if it doesn't.
+if (!fs.existsSync(outputDirectory)) {
+  fs.mkdirSync(outputDirectory, { recursive: true })
+}
 
 const tasks = Array.from({ length: 100 }, () => ({
   id: `TASK-${faker.number.int({ min: 1000, max: 9999 })}`,
   title: faker.hacker
     .phrase()
     .replace(/^./, (letter: string) => letter.toUpperCase()),
+  employee: faker.person.fullName(),
   status: faker.helpers.arrayElement(statuses).value,
   label: faker.helpers.arrayElement(labels).value,
   priority: faker.helpers.arrayElement(priorities).value,
 }))
 
-fs.writeFileSync(
-  path.join(__dirname, 'data', 'tasks.json'),
-  JSON.stringify(tasks, null, 2),
-)
-
-console.log('__dirname:', path.join(__dirname, 'data', 'tasks.json'))
-
+fs.writeFileSync(outputPath, JSON.stringify(tasks, null, 2))
+console.log(outputPath)
 console.log('✅ Tasks data generated.')
